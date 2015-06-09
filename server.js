86e36ccb0db690155b3488c54ba3ca9bbbc108a7
Sanyam Kamat <sanyamkamat@gmail.com>
2015-06-09 17:12:55 +0530

    setup the server

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 /**
  * Created by sanyamkamat on 09-06-2015.
  */
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.json({message: 'Woohoo! Welcome to our api!'});
+});
+
+app.use('/api', router);
+
+app.listen(port);
+
+console.log("Server started on port " + port);
